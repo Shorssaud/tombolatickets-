091224a154e7d1669c11ed4caa886a31fda3c833
Shorssaud <shaun.orssaud@epitech.eu>
2021-09-30 15:33:17 +0200

    fixed accents

--- prepare_sentence.py
+++ prepare_sentence.py
@@ -2,12 +2,13 @@ from clean import clean, remove_useless_spaces
 import string
 from remove_stopwords import remove_stopwords
 from lemmatize import lemmatize
-
+from stringy import remove_accents
 
 def prepare_sentence(text):
     text = text.lower()
     text = clean(text)
     # text = lemmatize(text)
+    text = remove_accents(text)
     # text = remove_stopwords(text)
     text = remove_useless_spaces(text)
     return text
